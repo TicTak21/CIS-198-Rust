1190ce089411a558ce48c1f43790f2fb35b15ffd
Alex <tictak211@gmail.com>
2020-07-05 14:32:12 +0300

    target folder exclude

--- homework/hw00/target/debug/deps/hw00.d
+++
@@ -1,5 +0,0 @@
-/Users/alex_kovalev/Desktop/Projects/Rust/CIS-198-Rust/homework/hw00/target/debug/deps/hw00: src/main.rs
-
-/Users/alex_kovalev/Desktop/Projects/Rust/CIS-198-Rust/homework/hw00/target/debug/deps/hw00.d: src/main.rs
-
-src/main.rs:
--- homework/hw00/target/debug/hw00.d
+++
@@ -1,1 +0,0 @@
-/Users/alex_kovalev/Desktop/Projects/Rust/CIS-198-Rust/homework/hw00/target/debug/hw00: /Users/alex_kovalev/Desktop/Projects/Rust/CIS-198-Rust/homework/hw00/src/main.rs
